451a556c245d93a1e3105b3c6f171a6924b711fe
Kabila Haile Soboka <kabilahaile@gmail.com>
2020-08-20 22:07:42 +0300

    Express Server

---
+++ app.js
@@ -0,0 +1,31 @@
+//Express
+const express = require('express');
+const  app = express();
+//Body parser
+const body_parser = require('body-parser');const e = require('express');
+
+//Path
+const path = require('path');
+
+//JSON Parser
+app.use(
+    body_parser.urlencoded({
+        extended:true
+    })
+)
+
+//Middle Wares
+app.use(body_parser.json());
+app.use((req,res,next)=>{
+    next()
+});
+
+//Handle routes
+app.route("/").get((req,res)=>{
+    res.sendFile(path.resolve('./index.html'));
+});
+
+let port = 3000;
+app.listen(port,()=>{
+    console.log("Server Started on Port: " + port);
+});
