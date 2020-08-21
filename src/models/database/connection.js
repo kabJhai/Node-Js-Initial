0ed11f38bcf98972996d54c7ad26dfb0e708e6bd
Kabila Haile Soboka <kabilahaile@gmail.com>
2020-08-21 15:19:10 +0300

    Make a database connection

---
+++ src/models/database/connection.js
@@ -0,0 +1,9 @@
+//Make a database connection
+const Sequelize = require('sequelize');
+//Sequelize("database_name","user_name","password",{host:'127.0.0.1',dialect:'mysql',operatorAliases:false})
+const sequelize = new Sequelize("social_network","root","root",{host:'127.0.0.1',dialect:'mysql',operatorAliases:false});
+
+module.exports = sequelize;
+
+//Make the sequelize object global
+global.sequelize = sequelize;
